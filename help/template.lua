d55814f3054c55bc3fc8c86aad1a48a92b465307
mcalibk <bkieranmcauliffe@gmail.com>
2023-05-22 15:13:46 -0400

    it works

--- help/template.lua
+++ help/template.lua
@@ -1,20 +1,33 @@
 -- basic template for usage
 
+
+
 local granmodule = {}
 granmodule.state = {}
-local matrix = require "lib/matrix"
+local mymod = require("mymod")
+
+function randrange(m, n)
+    return math.random() * (n - m) + m
+end
+
+function octfreq(linocts)
+    return 2^linocts
+end
 
 function granmodule.init()
-    print("Running init method")
+    granmodule.state.rate = 10
+    post("Running init method")
+    post(package.path)
+    mymod.myfunc();
     -- setup initial values for the state at the start
 end
 
 function granmodule.generate()
-    
+    --post("new grain");
     -- create parameters for a grain and modify state if needed
-    rate = .01
-    dur = 0.01
-    freq = 440
+    rate = granmodule.state.rate
+    dur = 100
+    freq = octfreq(randrange(7, 14))
     amp = 1
     pan = 0.5
 
@@ -23,6 +36,8 @@ end
 
 function granmodule.update(...)
     -- receives updates from lists sent to the object as args
+    granmodule.state.rate = ...
 end
 
 return granmodule
+
